5388d4e211b36893e3c6bd1a6c6fc90ab78bdfd3
samuel413 <44417462+samuel413@users.noreply.github.com>
2018-12-13 15:24:14 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$('input').val();
+
+$('button').click(function() {
+    if ($('input').val().toLowerCase() === 'eclipse') {
+        alert('AlIbay8.github.io/Cherished-Items-Blog');
+    } else {
+            alert('this is a hint. the capital letters are also a hint.');
+            alert('thoUgh letter caN mean two thIngs');
+            alert('his father has one Child');
+            alert('lOng range Dark tomE fe7');
+    }
+});
